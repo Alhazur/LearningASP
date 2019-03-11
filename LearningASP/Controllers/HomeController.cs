599fac9fec6d44c9e1d54ce44b5ff1366383a6d4
deltagare <deltagare@LEXJONKAF32>
2019-03-11 16:22:16 +0100

    Add project files.

---
+++ LearningASP/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningASP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LearningASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Swing()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Swing(string angle, string velocity)
+        {
+            double numAngle = 0;
+            double numVelocity = 0;
+
+            angle = angle.Replace(".", ",");
+            velocity = velocity.Replace(".", ",");
+
+            double.TryParse(angle, out numAngle);
+            double.TryParse(velocity, out numVelocity);
+
+            ViewBag.Distance = Golf.Distance(numAngle, numVelocity);//klass golf + method
+
+            return View();
+        }
+    }
+}
